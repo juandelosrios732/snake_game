d8260729cf89a7eefec0781f1b12147343e42588
juandelosrios732 <139810018+juandelosrios732@users.noreply.github.com>
2023-12-23 10:36:24 -0300

    Add files via upload

---
+++ snake/js/main.js
@@ -0,0 +1,94 @@
+const playBoard = document.querySelector(".play-board");
+
+let gameOver = false;
+let foodX, foodY;
+let snakeX = 5, snakeY = 10;
+let snakeBody = [];
+let velocityX = 0, velocityY = 0;
+let setIntervalId;
+
+// Función que genera la alatoriedad en el tablero de la comida que comerá la serpiente
+
+const changeFoodPosition = () => {
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+}
+
+// HandleGameOver, función para cuando el juego termina, por el choque con un borde o con el cuerpo de la serpiente, reseteo del intervalo de movimiento, alerta de juego terminado y recarga de la página para volver a iniciar el juego
+
+const handleGameOver = () => {
+    clearInterval(setIntervalId);
+    alert("Juego Terminado");
+    location.reload();
+}
+
+// Movimiento de la serpiente con las teclas arrow, utilizando las variables velocity, impidiendo que la serpiente de marcha atrás
+
+const changeDirection = (e) => {
+ //console.log(e);
+ if (e.key === "ArrowUp" && velocityY != 1) {
+    velocityX = 0;
+    velocityY = -1;
+ } else if (e.key === "ArrowDown" && velocityY != -1) {
+    velocityX = 0;
+    velocityY = 1;
+ } else if (e.key === "ArrowLeft" && velocityX != 1) {
+    velocityX = -1;
+    velocityY = 0;
+ } else if (e.key === "ArrowRight" && velocityX != -1) {
+    velocityX = 1;
+    velocityY = 0;
+ }
+//  initGame();
+}
+
+// Creación de la clase food, cuadrícula en la que estará ubicada la comida, y creación de la posición inicial de la serpiente, dando inicio al juego con la función "initGame()" 
+
+const initGame = () => {
+    if(gameOver) return handleGameOver();
+    let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+
+    // Comer Fruta: al momento que la serpiente pasa sobre la comida, una nueva comida aparecerá aleatoriamente en el tablero, además hace crecer el array snakeBody con el método push:
+    if (snakeX === foodX && snakeY === foodY) {
+        changeFoodPosition();
+        snakeBody.push([foodX, foodY]);
+        console.log(snakeBody);
+    }
+
+    // Arreglando el crecimiento de la serpiente: al momento de pasar la serpiente sobre la comida va a correr el cuadrado hacia atras, creando y haciendo crecer el cuerpo de la serpiente.
+    for(let i = snakeBody.length -1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+
+
+    // Serpiente Inicial:
+    snakeBody[0] = [snakeX, snakeY];
+
+    // Generación del movimiento de la serpiente, junto con los valores iniciales y la escucha del presionar teclas correspondientes
+
+    snakeX += velocityX;
+    snakeY += velocityY;
+
+    // Choque contra la pared, si la serpiente se choca con algún borde del tablero el juego se termina
+
+    if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+        gameOver = true
+    }
+
+
+    // Hacer crecer el cuerpo de la serpiente con el método for, cada vez que la serpiente pase por encima de una comida se va a estar creando un nuevo div: 
+    for (let i = 0; i < snakeBody.length; i++) {
+        htmlMarkup += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+        if(i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+            gameOver = true;
+        }
+    }
+    playBoard.innerHTML = htmlMarkup;
+}
+
+changeFoodPosition();
+//initGame();
+// Generar movimiento automático de la serpiente a partir que se inicia el juego con setInterval
+setIntervalId = setInterval(initGame, 125);
+
+document.addEventListener("keydown", changeDirection);
